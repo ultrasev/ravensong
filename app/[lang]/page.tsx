ae525e5376216cbc72444c55b34890944f11f61f
ultrasev <i@ultrasev.com>
2024-11-04 14:56:20 +0800

    feat: add ShortcodeDoc component

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,9 +1,9 @@
 import { Locale } from "@/lib/i18n-config";
-
+import { goblin } from "@/app/ui/Font";
 export const runtime = "edge";
 function CodeExample({ code }: { code: string }) {
   return (
-    <div className="relative">
+    <div className="relative mb-8">
       <div className="absolute top-3 left-4 flex items-center gap-1">
         <div className="h-3 w-3 rounded-full bg-red-500"></div>
         <div className="h-3 w-3 rounded-full bg-yellow-500"></div>
@@ -16,6 +16,30 @@ function CodeExample({ code }: { code: string }) {
   );
 }
 
+const ShortcodeDoc = () => {
+  const shortcode_example = "https://raven.cufo.cc/shortcode?title=xxx";
+  return (
+    <div className="mt-4">
+      <h2 className={`text-2xl font-bold ${goblin.className}`}> Shortcode</h2>
+      <p>
+        By manipulating middleware forwarding rules, you can use shortcode to
+        send notification. The shortcode is defined in cloudflare secrets.
+      </p>
+      <CodeExample code={shortcode_example.toString()} />
+      <h3 className={`text-xl font-bold ${goblin.className}`}>
+        Shortcode formats
+      </h3>
+      <ul className="list-disc list-inside">
+        <li>platform: e.g., b(bark), t(telegram)</li>
+        <li>user shortname: e.g., al(alice), bob(bob)</li>
+        <li>device: e.g., ip10(iphone 10), s20(samsung s20)</li>
+      </ul>
+      E.g., <span className="text-pink-500"> bbobs20 </span> represents sending
+      a message to bob&apos;s samsung s20 via bark.
+    </div>
+  );
+};
+
 export default async function Home({ params }: { params: { lang: Locale } }) {
   const code1 =
     "curl -X POST https://raven.cufo.cc/api/token/platform?title=title&message=message";
@@ -24,7 +48,7 @@ export default async function Home({ params }: { params: { lang: Locale } }) {
   return (
     <div className="flex items-center justify-center max-w-5xl mx-auto">
       <div className="flex flex-col gap-4">
-        <h1 className="text-4xl font-bold">Usage</h1>
+        <h1 className={`text-4xl font-bold ${goblin.className}`}>Usage</h1>
         <CodeExample code={code1.toString()} />
         <p>
           For example, if you&apos;re using{" "}
@@ -32,6 +56,7 @@ export default async function Home({ params }: { params: { lang: Locale } }) {
           icon.
         </p>
         <CodeExample code={code2.toString()} />
+        <ShortcodeDoc />
       </div>
     </div>
   );
